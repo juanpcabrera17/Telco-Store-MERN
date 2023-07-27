7036dee8926caeef3e14b9267ea2b0e2067e8a18
juanpcabrera17 <juanpic17@gmail.com>
2023-07-27 17:37:09 -0300

    redirect on empty fav & orders

--- client/src/pages/Favorites.jsx
+++ client/src/pages/Favorites.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import { useUser } from '../context/UserContext';
 
 export const Favorites = () => {
 	const { user, toggleFavorite } = useUser();
-	console.log(user);
+	console.log(user.favorites);
 
 	return (
 		<div className="flex items-center flex-col ">
@@ -13,63 +14,78 @@ export const Favorites = () => {
 				<p className="text-gray-500">Manage your favorite products</p>
 			</div>
 
-			<div className="w-3/4 relative overflow-x-auto shadow-md sm:rounded-lg">
-				<table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-					<thead className="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
-						<tr>
-							<th scope="col" className="px-6 py-3">
-								<span className="sr-only">Image</span>
-							</th>
-							<th scope="col" className="px-6 py-3">
-								Product
-							</th>
-							<th scope="col" className="px-6 py-3">
-								Description
-							</th>
-							<th scope="col" className="px-6 py-3">
-								Category
-							</th>
-							<th scope="col" className="px-6 py-3">
-								Price
-							</th>
-							<th scope="col" className="px-6 py-3"></th>
-						</tr>
-					</thead>
-					<tbody>
-						{user.favorites.map((product) => (
-							<tr
-								key={product._id}
-								className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
-							>
-								<td className="w-32 p-4">
-									<img src={product.thumbnail} alt="Apple Watch" />
-								</td>
-								<td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-									{product.name}
-								</td>
-								<td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-									*Description*
-								</td>
-								<td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-									{product.category}
-								</td>
-								<td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-									${product.price}
-								</td>
-								<td className="px-6 py-4 font-semibold ">
-									<button
-										onClick={() => toggleFavorite(product._id, product)}
-										href="#"
-										className="font-medium text-red-600 dark:text-red-500 hover:underline"
-									>
-										Remove
-									</button>
-								</td>
+			{user.favorites == '[]' ? (
+				<div className="w-3/4 relative overflow-x-auto shadow-md sm:rounded-lg">
+					<table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+						<thead className="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
+							<tr>
+								<th scope="col" className="px-6 py-3">
+									<span className="sr-only">Image</span>
+								</th>
+								<th scope="col" className="px-6 py-3">
+									Product
+								</th>
+								<th scope="col" className="px-6 py-3">
+									Description
+								</th>
+								<th scope="col" className="px-6 py-3">
+									Category
+								</th>
+								<th scope="col" className="px-6 py-3">
+									Price
+								</th>
+								<th scope="col" className="px-6 py-3"></th>
 							</tr>
-						))}
-					</tbody>
-				</table>
-			</div>
+						</thead>
+						<tbody>
+							{user.favorites.map((product) => (
+								<tr
+									key={product._id}
+									className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+								>
+									<td className="w-32 p-4">
+										<img src={product.thumbnail} alt="Apple Watch" />
+									</td>
+									<td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
+										{product.name}
+									</td>
+									<td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
+										*Description*
+									</td>
+									<td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
+										{product.category}
+									</td>
+									<td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
+										${product.price}
+									</td>
+									<td className="px-6 py-4 font-semibold ">
+										<button
+											onClick={() => toggleFavorite(product._id, product)}
+											href="#"
+											className="font-medium text-red-600 dark:text-red-500 hover:underline"
+										>
+											Remove
+										</button>
+									</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+				</div>
+			) : (
+				<div>
+					<p>You have no favorites</p>
+					<Link to="/shop" className="flex font-semibold text-indigo-600 text-sm mt-10">
+						<svg
+							className="fill-current mr-2 text-indigo-600 w-4"
+							viewBox="0 0 448 512"
+						>
+							<path d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z"></path>
+						</svg>
+						Keep buying
+					</Link>
+				</div>
+			)}
 		</div>
 	);
 };
--- client/src/pages/Orders.jsx
+++ client/src/pages/Orders.jsx
@@ -30,7 +30,6 @@ export const Orders = () => {
 			}
 		};
 		fetchData();
-		console.log(orders);
 	}, []);
 
 	return (
@@ -41,74 +40,89 @@ export const Orders = () => {
 					Check the status of recent and old orders & discover more products
 				</p>
 			</div>
-			{orders.map((order) => (
-				<div className="w-3/4 rounded-md border flex mb-10" key={order._id}>
-					<div className="flex p-8 flex-col w-1/3 bg-gray-100">
-						<div className="pb-5">
-							<p className="text-gray-500">Order ID</p>
-							<p className="font-medium">{order._id}</p>
+			{orders ? (
+				orders.map((order) => (
+					<div className="w-3/4 rounded-md border flex mb-10" key={order._id}>
+						<div className="flex p-8 flex-col w-1/3 bg-gray-100">
+							<div className="pb-5">
+								<p className="text-gray-500">Order ID</p>
+								<p className="font-medium">{order._id}</p>
+							</div>
+							<div className="pb-5">
+								<p className="text-gray-500"> Date </p>
+								<p className="font-medium">{getDate(order.createdAt)}</p>
+							</div>
+							<div className="pb-5">
+								<p className="text-gray-500"> Total Amount </p>
+								<p className="font-medium">
+									{order.checkout.total + order.shippingData.shippingCharge}
+								</p>
+							</div>
+							<div className="pb-5">
+								<p className="text-gray-500"> Order Status </p>
+								<p className="font-medium flex items-center">
+									{order.status === 'Pending' ? (
+										<AiOutlineClockCircle className="mr-2 text-yellow-600 text-lg" />
+									) : (
+										<AiOutlineCheckCircle className="mr-2 text-green-600 text-lg" />
+									)}
+									{order.status}
+								</p>
+							</div>
 						</div>
-						<div className="pb-5">
-							<p className="text-gray-500"> Date </p>
-							<p className="font-medium">{getDate(order.createdAt)}</p>
-						</div>
-						<div className="pb-5">
-							<p className="text-gray-500"> Total Amount </p>
-							<p className="font-medium">
-								{order.checkout.total + order.shippingData.shippingCharge}
-							</p>
-						</div>
-						<div className="pb-5">
-							<p className="text-gray-500"> Order Status </p>
-							<p className="font-medium flex items-center">
-								{order.status === 'Pending' ? (
-									<AiOutlineClockCircle className="mr-2 text-yellow-600 text-lg" />
-								) : (
-									<AiOutlineCheckCircle className="mr-2 text-green-600 text-lg" />
-								)}
-								{order.status}
-							</p>
+						<div className="p-8 w-2/3">
+							<ul className="border-b pb-10">
+								{order.checkout.cart.map((item) => (
+									<li className="flex mb-8" key={item._id}>
+										<img
+											className=" w-1/4 mr-6 rounded-md border object-cover object-center"
+											src={item.thumbnail}
+											alt=""
+										/>
+										<div className="flex flex-col justify-between w-full">
+											<div>
+												<p className="font-semibold">{item.name}</p>
+												<p className="text-gray-500">{item.category}</p>
+											</div>
+											<div>
+												<Link
+													to={`/itemdetail/${item._id}`}
+													className="text-gray-500 hover:text-black"
+												>
+													View Product
+												</Link>
+												<span className="mx-2 text-gray-200">|</span>
+												<Link
+													to={`/shop/${item.category}`}
+													className="text-gray-500 hover:text-black"
+												>
+													Similar Product
+												</Link>
+											</div>
+										</div>
+										<div className="flex justify-end w-full">
+											<span className="font-bold">${item.price}</span>
+										</div>
+									</li>
+								))}
+							</ul>
 						</div>
 					</div>
-					<div className="p-8 w-2/3">
-						<ul className="border-b pb-10">
-							{order.checkout.cart.map((item) => (
-								<li className="flex mb-8" key={item._id}>
-									<img
-										className=" w-1/4 mr-6 rounded-md border object-cover object-center"
-										src={item.thumbnail}
-										alt=""
-									/>
-									<div className="flex flex-col justify-between w-full">
-										<div>
-											<p className="font-semibold">{item.name}</p>
-											<p className="text-gray-500">{item.category}</p>
-										</div>
-										<div>
-											<Link
-												to={`/itemdetail/${item._id}`}
-												className="text-gray-500 hover:text-black"
-											>
-												View Product
-											</Link>
-											<span className="mx-2 text-gray-200">|</span>
-											<Link
-												to={`/shop/${item.category}`}
-												className="text-gray-500 hover:text-black"
-											>
-												Similar Product
-											</Link>
-										</div>
-									</div>
-									<div className="flex justify-end w-full">
-										<span className="font-bold">${item.price}</span>
-									</div>
-								</li>
-							))}
-						</ul>
-					</div>
+				))
+			) : (
+				<div>
+					<p>You have no orders</p>
+					<Link to="/shop" className="flex font-semibold text-indigo-600 text-sm mt-10">
+						<svg
+							className="fill-current mr-2 text-indigo-600 w-4"
+							viewBox="0 0 448 512"
+						>
+							<path d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z"></path>
+						</svg>
+						Keep buying
+					</Link>
 				</div>
-			))}
+			)}
 		</div>
 	);
 };
